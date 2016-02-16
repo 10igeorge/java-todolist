f0c1bf9480d8462816384cbf8b92f851455c9c9a
isabelle george and kevin mattison <10izzygeorge@gmail.com>
2016-02-16 08:38:00 -0800

    Create files and testing for todo list.

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+  @Test
+  public void Task_instantiatesCorrectly_true(){
+    Task taskApp = new Task("Mow the lawn");
+    assertEquals(true, taskApp instanceof Task);
+  }
+
+  @Test
+  public void task_instantiatesWithdescription_true() {
+    Task taskApp = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+}
